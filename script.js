b7447a4d7a868a24b4e3b3f2557de364036042fb
phantomramen <ali.hernandez9two@gmail.com>
2020-07-12 02:08:06 -0600

    added promps and related funtions, if/else statements.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function generatePassword() {
 
 var generateBtn = document.querySelector("#generate");
-// all available characters and length
 var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
 var passwordLength = 16;
 var password = "";
@@ -13,28 +12,9 @@ for (var i=0; i<passwordLength; i++){
   }
   document.getElementById("password").value = password;
 }
-//Code for slider
-const allRanges = document.querySelectorAll(".range-wrap");
-allRanges.forEach(wrap => {
-  const range = wrap.querySelector(".range");
-  const bubble = wrap.querySelector(".bubble");
 
-  range.addEventListener("input", () => {
-    setBubble(range, bubble);
-  });
-  setBubble(range, bubble);
-});
 
-function setBubble(range, bubble) {
-  const val = range.value;
-  const min = range.min ? range.min : 0;
-  const max = range.max ? range.max : 100;
-  const newVal = Number(((val - min) * 100) / (max - min));
-  bubble.innerHTML = val;
 
-  // Sorta magic numbers based on size of the native UI thumb
-  bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
-}
 
 // Write password to the #password input
 function writePassword() {
